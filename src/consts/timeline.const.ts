6d90eddfe584d20cb00032c5562fcfcca37e0447
Felipe Zeba <felipe_zeba@outlook.com>
2026-02-23 17:58:32 +0900

    feat: add timeline items as constant

---
+++ src/consts/timeline.const.ts
@@ -0,0 +1,44 @@
+export const timelineItems: TTimeLine[] = [
+  {
+    title: "情報システム学学士",
+    company: "エスタシオ・デ・サ大学",
+    date: "2020",
+    location: "ブラジル・リオデジャネイロ",
+    image: "/images/logos/estacio-logo.webp",
+  },
+  {
+    title: "フロントエンドエンジニア",
+    company: "V8 Tech",
+    date: "2021",
+    location: "ブラジル・サンパウロ",
+    image: "/images/logos/v8-logo.jpeg",
+  },
+  {
+    title: "ソフトウェアエンジニア",
+    company: "HP Inc（ Kforce Inc. 経由 ）",
+    date: "2022",
+    location: "アメリカ・フロリダ",
+    image: "/images/logos/hp-logo.jpg",
+  },
+  {
+    title: "コンピュータサイエンス修士",
+    company: "マハリシ国際大学",
+    date: "2024",
+    location: "アメリカ・アイオワ",
+    image: "/images/logos/maharishi-logo.png",
+  },
+  {
+    title: "フロントエンドエンジニア",
+    company: "V8 Tech",
+    date: "2024",
+    location: "ブラジル・サンパウロ",
+    image: "/images/logos/v8-logo.jpeg",
+  },
+  {
+    title: "JLPT N3",
+    description: "日本語能力試験N3に合格",
+    date: "2025",
+    location: "日本・横浜",
+    image: "/images/logos/jlpt-logo.webp",
+  },
+]
---
+++ src/types/timeline.d.ts
@@ -0,0 +1,8 @@
+declare type TTimeLine = {
+  title: string;
+  company?: string;
+  date: string;
+  location: string;
+  description?: string;
+  image: string;
+}
