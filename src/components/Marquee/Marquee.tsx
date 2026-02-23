1564df3848b31eb4dd6ee9ce48ed9efe73c83b38
Felipe Zeba <felipe_zeba@outlook.com>
2026-02-23 22:06:18 +0900

    refactor: make Track a separate component

--- src/components/Marquee/Marquee.tsx
+++ src/components/Marquee/Marquee.tsx
@@ -4,45 +4,31 @@ interface MarqueeProps {
   items: string[];
 }
 
+interface TrackProps {
+  items: string[];
+  ariaHidden?: boolean;
+}
+
+const Track: React.FC<TrackProps> = ({ items, ariaHidden = false }) => {
+  return (
+    <div aria-hidden={ariaHidden} className={styles.track}>
+        {items.map((item, index) => (
+          <div className={styles.group}>
+            <span key={`a-${index}`} className={styles.item}>
+              {item}
+            </span>
+          </div>
+        ))}
+    </div>
+  );
+};
+
 const Marquee: React.FC<MarqueeProps> = ({ items }) => {
   return (
     <div className={styles.marquee}>
-      <div className={styles.track}>
-          {items.map((item, index) => (
-            <div className={styles.group}>
-              <span key={`a-${index}`} className={styles.item}>
-                {item}
-              </span>
-            </div>
-          ))}
-      </div>
-      <div aria-hidden className={styles.track}>
-          {items.map((item, index) => (
-            <div className={styles.group}>
-              <span key={`a-${index}`} className={styles.item}>
-                {item}
-              </span>
-            </div>
-          ))}
-      </div>
-      <div aria-hidden className={styles.track}>
-          {items.map((item, index) => (
-            <div className={styles.group}>
-              <p key={`a-${index}`} className={styles.item}>
-                {item}
-              </p>
-            </div>
-          ))}
-      </div>
-      <div aria-hidden className={styles.track}>
-          {items.map((item, index) => (
-            <div className={styles.group}>
-              <p key={`a-${index}`} className={styles.item}>
-                {item}
-              </p>
-            </div>
-          ))}
-      </div>
+      <Track items={items} />
+      <Track items={items} ariaHidden={true} />
+      <Track items={items} ariaHidden={true} />
     </div>
   );
 };
