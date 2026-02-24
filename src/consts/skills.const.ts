4310dd083dc9f3c7fc8fd529a89b2efbbacbf7c9
Felipe Zeba <felipe_zeba@outlook.com>
2026-02-24 14:04:46 +0900

    refactor: update skills const

--- src/consts/skills.const.ts
+++ src/consts/skills.const.ts
@@ -6,12 +6,12 @@ export const skills: {name: string; image: string}[] = [
   {name: "JavaScript", image: "/images/logos/techstack/javascript.png"},
   {name: "C#", image: "/images/logos/techstack/c-sharp.png"},
   {name: "HTML", image: "/images/logos/techstack/html.png"},
-  //TODO: uncomment these when I have the icons ready
-  // {name: "CSS", image: "/images/logos/techstack/css.png"},
-  // {name: "Sass", image: "/images/logos/techstack/sass.png"},
-  // {name: "Webpack", image: "/images/logos/techstack/webpack.png"},
-  // {name: "GraphQL", image: "/images/logos/techstack/graphql.png"},
-  // {name: "Jest", image: "/images/logos/techstack/jest.png"},
-  // {name: "Git", image: "/images/logos/techstack/git.png"},
-  // {name: "Agile Methodologies", image: "/images/logos/techstack/agile.png"},
+  //TODO: update image path when I have the icons ready
+  {name: "CSS", image: "/images/logos/techstack/react.png"},
+  {name: "Sass", image: "/images/logos/techstack/redux.png"},
+  {name: "Webpack", image: "/images/logos/techstack/react-native.png"},
+  {name: "GraphQL", image: "/images/logos/techstack/typescript.png"},
+  {name: "Jest", image: "/images/logos/techstack/javascript.png"},
+  {name: "Git", image: "/images/logos/techstack/c-sharp.png"},
+  {name: "Agile Methodologies", image: "/images/logos/techstack/html.png"},
 ];
